3b7ad17050b0d6fecb61090702cd0c5ee87bd30b
myleskrisik <myleskrisik@gmail.com>
2024-06-05 19:44:04 -0700

    Got player rendering

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,21 @@
 const std = @import("std");
 const rl = @cImport(@cInclude("raylib.h"));
 
+const Subpixel = struct {
+    const SUBPIXEL_BITS = 4;
+    sp: i32 = 0,
+
+    pub fn getPixels(self: Subpixel) i32 {
+        return self.sp >> SUBPIXEL_BITS;
+    }
+};
+
 pub fn main() !void {
     const screen_width = 800;
     const screen_height = 450;
 
-    const virtual_screen_width = 160;
-    const virtual_screen_height = 90;
+    const virtual_screen_width = 320;
+    const virtual_screen_height = 180;
 
     const virtual_ratio = screen_width / virtual_screen_width;
 
@@ -20,28 +29,21 @@ pub fn main() !void {
     const target = rl.LoadRenderTexture(virtual_screen_width, virtual_screen_height);
     defer rl.UnloadRenderTexture(target);
 
-    const rect_0 = rl.Rectangle{ .x = 70, .y = 35, .width = 20, .height = 20 };
-    const rect_1 = rl.Rectangle{ .x = 90, .y = 55, .width = 30, .height = 10 };
-    const rect_2 = rl.Rectangle{ .x = 80, .y = 65, .width = 15, .height = 25 };
-
     const source_rect = rl.Rectangle{ .x = 0, .y = 0, .width = @floatFromInt(target.texture.width), .height = @floatFromInt(-target.texture.height) };
     const dest_rect = rl.Rectangle{ .x = -virtual_ratio, .y = -virtual_ratio, .width = screen_width + (virtual_ratio * 2), .height = screen_height + (virtual_ratio * 2) };
 
     const origin = rl.Vector2{ .x = 0, .y = 0 };
 
-    var rotation: f32 = 0;
-
-    var camera_x: f32 = 0;
-    var camera_y: f32 = 0;
+    const camera_x: f32 = 0;
+    const camera_y: f32 = 0;
 
     rl.SetTargetFPS(60);
 
+    const player_sprite = rl.LoadTexture("player.png");
+    const player_positon = .{ Subpixel{}, Subpixel{} };
+    defer rl.UnloadTexture(player_sprite);
+
     while (!rl.WindowShouldClose()) {
-        rotation += 60 * rl.GetFrameTime();
-
-        camera_x = @floatCast((std.math.sin(rl.GetTime()) * 50.0) - 10);
-        camera_y = @floatCast(std.math.cos(rl.GetTime()) * 30);
-
         screen_space_camera.target = rl.Vector2{ .x = camera_x, .y = camera_y };
 
         world_space_camera.target.x = screen_space_camera.target.x;
@@ -52,18 +54,18 @@ pub fn main() !void {
         screen_space_camera.target.y -= world_space_camera.target.y;
         screen_space_camera.target.y *= virtual_ratio;
 
+        // Draw worldspace
         {
             rl.BeginTextureMode(target);
             defer rl.EndTextureMode();
             rl.ClearBackground(rl.WHITE);
             {
                 rl.BeginMode2D(world_space_camera);
+                rl.DrawTexture(player_sprite, player_positon[0].getPixels(), player_positon[1].getPixels(), rl.WHITE);
                 defer rl.EndMode2D();
-                rl.DrawRectanglePro(rect_0, origin, rotation, rl.BLACK);
-                rl.DrawRectanglePro(rect_1, origin, -rotation, rl.RED);
-                rl.DrawRectanglePro(rect_2, origin, rotation + 45, rl.BLUE);
             }
         }
+        // Draw screenspace
         {
             rl.BeginDrawing();
             defer rl.EndDrawing();
